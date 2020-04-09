7887a7f92bdc332fcd9281252cfac75ec2fbb786
Sergiogd112 <sergio.gomezdamas@gmail.com>
2020-04-09 21:52:09 +0200

    update

--- Datagraber.py
+++ Datagraber.py
@@ -14,7 +14,7 @@ import dateutil.relativedelta as dr
 import pandas as pd
 from urllib import request
 import os
-
+from Symbol import Symbol
 # In[14]:
 
 
@@ -62,51 +62,6 @@ def git_push():
         origin.push()
     except:
         print('Some error occured while pushing the code')    
-
-class Symbol:
-    def __init__(self,data,tick):
-        self.tick=tick
-        self.data=data
-    def from_url(ohlcurl,macdurl, interval,tick):
-        ohlctmp=json.loads(http.request('GET', ohlcurl).data)
-        print(ohlctmp.keys())
-        try:
-            ohlcdata=ohlctmp['Time Series ('+interval+')']
-        except Exception as e:
-            print(ohlctmp(list(ohlctmp.keys())[0]))
-            print(e.traceback)
-        time.sleep(12)
-        try:
-            macddata=json.loads(http.request('GET',macdurl).data)['Technical Analysis: MACDEXT']
-        except:
-            print(json.loads(http.request('GET',macdurl).data))
-        data={}
-        print(len(ohlcdata))
-        for keyo,keym in zip(list(ohlcdata.keys()),list(macddata.keys())):
-            if(keyo[:-3] in list(macddata.keys())):
-                data[keyo[:-3]]={**ohlcdata[keyo],**macddata[keyo[:-3]]}
-        time.sleep(12)
-        return Symbol(data,tick)
-    def update(self,ohlcurl,macdurl,interval):
-        ohlctmp=json.loads(http.request('GET', ohlcurl).data)
-        print(ohlctmp.keys())
-        ohlcdata=ohlctmp['Time Series ('+interval+')']
-        time.sleep(12)
-        try:
-            macddata=json.loads(http.request('GET',macdurl).data)['Technical Analysis: MACDEXT']
-        except:
-            print(json.loads(http.request('GET',macdurl).data))
-        data={}
-
-        print(len(ohlcdata))
-        for keyo,keym in zip(list(ohlcdata.keys()),list(macddata.keys())):
-            if(keyo[:-3] in list(macddata.keys())):
-                data[keyo[:-3]]={**ohlcdata[keyo],**macddata[keyo[:-3]]}
-        time.sleep(12)
-        for key in list(data.keys()):
-            if(not(key in list(self.data.keys()))):
-                self.data[key]=data[key]
-
 
 # In[17]:
 
